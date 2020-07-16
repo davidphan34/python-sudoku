a7d226720b4b5d5288bf4476992b8f6d3666258d
davidphan34 <davidphan34@gmail.com>
2020-07-16 00:45:33 -0700

    Added a couple lines to setup

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 #new python file to create sudoku file
 #testing to make sure that github is working
+board = [
+	[7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def print_board(bo):
+	for i in range(len(bo)):
+		if i % 3 and i != 0:
+			print("- - - - - - - - - - - - -") # horizontal line in order to separate the 9x9 blocks of numbers
+
+		for j in range(len(bo[0])):
+			if j % 3 == 0 and j != 0:
+				print(" | ", end="") # vertical line for same purpose, end = "" makes sure that the print statement does not include a \n
+
+			if j == 8:
+				print(bo[i][j]) #ensure that the \n is included
+			else:
+				print(bo([i][j]) + " ", end="")
+
+print_board(board)
